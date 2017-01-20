2f65edd993a4e3c8601d213eb586d2b21d9d5776
Ozan Kepenek <ozankepenek@gmail.com>
2017-01-20 18:21:30 +0300

    search result

--- food/urls.py
+++ food/urls.py
@@ -19,5 +19,6 @@ from main import views
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^search/$', views.search, name='search')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
 	return render(request, 'index.html', {'name': 'Ozan'})
+
+def search(request):
+	search_query = request.GET.get('squery')
+	search_url = 'https://api.nal.usda.gov/ndb/search/?format=json&sort=n&max=100&offset=0&api_key=6P0TtTKUCWh2ZaJ6ahBG2HfCz5HUWlPpapkNZEyH&q={}'.format(search_query)
+	search_results = requests.get(search_url)
+	return render(request, 'search.html', {'search_query': search_query, 'search_results': search_results.json()})
+
+
+
